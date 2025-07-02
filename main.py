dca1b01575ee95b5fb8e25b0b89e1a8e20fc570d
Ashmin Jayson <ashminjayson10@gmail.com>
2025-07-02 12:50:54 +0530

    chore: revert to scaffold

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-from routers.users import router as usersRouter
-from routers.todos import router as todosRouter
 
 app = FastAPI()
 
@@ -8,7 +6,3 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the To-Do API move to /docs for swagger documentation"}
-
-
-app.include_router(usersRouter)
-app.include_router(todosRouter)
