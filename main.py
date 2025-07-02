f6fe87dbbdbe5bfa49208bcc9a000e9c709e95e8
Ashmin Jayson <ashminjayson10@gmail.com>
2025-07-02 11:58:18 +0530

    feat: added router segregation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Form
-from data.user import users
-from data.todo import toDOS
+from fastapi import FastAPI
+from routers.users import router as usersRouter
+from routers.todos import router as todosRouter
 
 app = FastAPI()
 
@@ -10,37 +10,5 @@ def read_root():
     return {"message": "Welcome to the To-Do API move to /docs for swagger documentation"}
 
 
-@app.get("/users/{user_id}")
-def get_user_data(user_id: int):
-    print("Fetching user data with user ID", user_id)
-    for user in users:
-        if user["id"] == user_id:
-            print("User found:", user)
-            return user
-
-    print("User not found with ID", user_id)
-    return {"error": "User not found"}
-
-
-@app.get("/todos/{user_id}")
-def get_user_todos(user_id: int, limit: int, isNotCompleted: bool):
-    user_todos = [todo for todo in toDOS if todo['user_id'] == user_id]
-    if isNotCompleted:
-        user_todos = [
-            todo for todo in user_todos if todo['is_completed'] is False]
-    return user_todos[:limit]
-
-
-@app.post("/todos/{user_id}")
-def add_new_todo(user_id: int, title: str = Form(...), description: str = Form(...), due_date: str = Form(...)):
-    newTask = {
-        'id': 232,
-        'user_id': user_id,
-        'title': title,
-        'description': description,
-        'due_date': due_date,
-        'is_completed': False
-    }
-
-    toDOS.append(newTask)
-    return {"message": "New task created", "data": newTask}
+app.include_router(usersRouter)
+app.include_router(todosRouter)
